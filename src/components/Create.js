d6bfea5617311568bf43eec4cfa9039e5e9d8035
DANIEL GALLAGHER - STUDENT <G00360986@gmit.ie>
2019-10-23 10:28:43 +0100

    first commit

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,82 +1,103 @@
 import React, { Component } from 'react';
 import '../App.css'
+import axios from 'axios';
+import { timingSafeEqual } from 'crypto';
 
 export class Create extends Component {
 
-        constructor(props) {
-        super(props);
-        this.state = {Title: '',
-                      Year:  '',
-                      Poster: ''};
-    
-        this.handleChangeMovieTitle = this.handleChangeMovieTitle.bind(this);
-        this.handleChangeMovieYear = this.handleChangeMovieYear.bind(this);
-        this.handleChangeMoviePoster = this.handleChangeMoviePoster.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      Title: '',
+      Year: '',
+      Poster: ''
+    };
 
-      handleChangeMovieTitle(e) {
-        this.setState({Title: e.target.value});
-      }
+    this.handleChangeMovieTitle = this.handleChangeMovieTitle.bind(this);
+    this.handleChangeMovieYear = this.handleChangeMovieYear.bind(this);
+    this.handleChangeMoviePoster = this.handleChangeMoviePoster.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      handleChangeMovieYear(e) {
-        this.setState({Year: e.target.value});
-      }
+  handleChangeMovieTitle(e) {
+    this.setState({ Title: e.target.value });
+  }
 
-      handleChangeMoviePoster(e) {
-        this.setState({Poster: e.target.value});
-      }
-    
-      handleSubmit(e) {
-        alert('Movie Title: ' + this.state.Title + "  "+'Release Year: ' + this.state.Year + "  "+'Poster URL: ' + this.state.Poster);
-        e.preventDefault();
-      }
+  handleChangeMovieYear(e) {
+    this.setState({ Year: e.target.value });
+  }
 
-    render() {
-        return (           
-            <div>
-                <h1>Movie Submit Form</h1>
-                <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                <label>
-                 Movie Title:
-                <input 
-                type="text" 
-                value={this.state.Title} 
-                className = "form-control"
-                onChange={this.handleChangeMovieTitle} />
-                </label>
-                </div>
+  handleChangeMoviePoster(e) {
+    this.setState({ Poster: e.target.value });
+  }
 
-                <div className="form-group">
-                <label>
-                 Movie Year:
-                <input 
-                type="text" 
-                value={this.state.Year} 
-                className = "form-control"
-                onChange={this.handleChangeMovieYear} />
-                </label>
-                </div>
+  handleSubmit(e) {
+    alert('Movie Title: ' + this.state.Title + "  " + 'Release Year: ' + this.state.Year + "  " + 'Poster URL: ' + this.state.Poster);
+    e.preventDefault();
 
-                <div className="form-group">
-                    <label>
-                        Movie Poster URL:
-                        <textarea
-                            rows='3'
-                            className='form-control'
-                            value={this.state.Poster}
-                            onChange={this.handleChangeMoviePoster}>
-                        </textarea>
-                    </label>
-                </div>
-                
-            <div>
-                <input type="submit" value="Submit" />
-             </div>
-            </form>
-        </div>
-        );
+    const newMovie = {
+      title: this.state.Title,
+      year: this.state.Year,
+      poster: this.state.Poster
     }
+
+    axios.post('http://localhost:4000/api/movies', newMovie)
+      .then()
+      .catch();
+
+    this.setState({
+      Title: "",
+      Year: "",
+      Poster: ""
+
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Submit Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>
+              Movie Title:
+                <input
+                type="text"
+                value={this.state.Title}
+                className="form-control"
+                onChange={this.handleChangeMovieTitle} />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>
+              Movie Year:
+                <input
+                type="text"
+                value={this.state.Year}
+                className="form-control"
+                onChange={this.handleChangeMovieYear} />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>
+              Movie Poster URL:
+                        <textarea
+                rows='3'
+                className='form-control'
+                value={this.state.Poster}
+                onChange={this.handleChangeMoviePoster}>
+              </textarea>
+            </label>
+          </div>
+
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 export default Create;
